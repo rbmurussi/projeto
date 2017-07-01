e160405d0385aca2b6895e4fd6e0851a724a096e
rbmurussi <rbmurussi@gmail.com>
2017-06-30 23:28:04 -0300

    asdf

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,25 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
+import com.mongodb.Mongo;
+
+@Configuration
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	public @Bean Mongo mongo() throws Exception {
+		return new com.mongodb.MongoClient("locahost");
+	}
+
+	public @Bean MongoTemplate mongoTemplate() throws Exception {
+		return new MongoTemplate(mongo(), "database");
 	}
 }
--- src/main/java/com/example/ServletInitializer.java
+++ src/main/java/com/example/ServletInitializer.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo/")
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	@Autowired
+	private CustomerRepository customerRepository;
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DemoApplication.class);
@@ -16,7 +20,32 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	
 	@RequestMapping("test")
 	public String test() {
-		return "test";
+		//customerRepository.deleteAll();
+
+		// save a couple of customers
+		customerRepository.save(new Customer("Alice", "Smith"));
+		customerRepository.save(new Customer("Bob", "Smith"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : customerRepository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(customerRepository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : customerRepository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+		
+		return "test " + customerRepository.count();
 	}
 
 }
